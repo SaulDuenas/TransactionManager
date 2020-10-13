87b524347ea4eb0f92b249b4fbcefac3f38f3a13
Saúl Dueñas B <saul.duenas@hotmail.com>
2020-10-13 01:30:46 -0500

    subiendo cambios que encontre en zip con fecha 20190127

---
+++ DPS/_pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,16 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		D:\GateTech\Internos\TransactionManager\DPS\TransactionManager
+	}
+
+	public static void main(String[] args) {
+		D:\GateTech\Internos\TransactionManager\DPS\TransactionManagerXMLD:\GateTech\Internos\TransactionManager\DPS\TransactionManagerXMLD:\GateTech\Internos\TransactionManager\DPS\TransactionManagerXML
+	}
+
+	public static void main(String[] args) {
+		D:\GateTech\Infra\JavaD:\GateTech\Infra\Java
+	}
+}
+
